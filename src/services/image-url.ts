68f26a8b147ce2f9d51e6ac928268bf9c40de6f8
HectorRussia <Ponkrit.wo@hotmail.com>
2024-03-19 02:59:49 +0700

    Handling Games without an Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/92bf08382f9b2540338040591d8dd329.jpg';
+
 const getCroppedImageUrl = (url:string) =>{
-    if(!url) return '';
+    if(!url) return noImage;
     const target = 'media/';
     const index = url.indexOf(target)+target.length;
     return url.slice(0,index)+'crop/600/400/'+url.slice(index);
