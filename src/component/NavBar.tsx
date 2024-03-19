c0e42a630c1539d4fe5dab660e5a35c0176e30d9
HectorRussia <Ponkrit.wo@hotmail.com>
2024-03-20 01:12:38 +0700

    Implement Searching Game

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -2,11 +2,16 @@ import { HStack,Image} from '@chakra-ui/react'
 import logo from '../assets/image0_0.jpg'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
-const NavBar = () => {
+
+interface Props {
+  onSearch: (searchText:string) => void;
+}
+
+const NavBar = ({onSearch}:Props) => {
   return (
    <HStack padding='10px'>
         <Image src={logo} boxSize='60px' marginLeft='10px'></Image>
-        <SearchInput/>
+        <SearchInput onSearch={onSearch}/>
         <ColorModeSwitch/>
    </HStack>
   )
