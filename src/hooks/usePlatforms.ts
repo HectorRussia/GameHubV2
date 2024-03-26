3ecb5d722f58185be484c4fb01977fe33188f755
HectorRussia <Ponkrit.wo@hotmail.com>
2024-03-27 03:32:48 +0700

    Fecthing Platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,20 @@
-import useData from "./useData";
-
+import { useQuery } from "@tanstack/react-query";
+import apiCilent from "../services/api-cilent";
+import { FetchRespone } from "./useData";
+import platforms from "../data/platforms";
 interface Platform {
     id:number;
     name:string;
     slug:string
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () =>
+         apiCilent.get<FetchRespone<Platform>>('/platforms/lists/parents')
+            .then(res=>res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: {count : platforms.length, results: platforms}
+});
 
 export default usePlatforms;
