c50f14ea8ada095d699f75a7be08271b08118ad5
HectorRussia <Ponkrit.wo@hotmail.com>
2024-04-13 00:12:57 +0700

    refactoring Entities

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,14 +3,9 @@ import genres from "../data/genres";
 import APIClient from "../services/api-cilent";
 import { useQuery } from "@tanstack/react-query";
 import ms from 'ms';
+import { Genre } from "../entities/Genre";
 
 const apiCilent = new APIClient<Genre>('/genres');
-export interface Genre {
-    id:number;
-    name:string;
-    image_background: string;
-}
-
 // const useGenres = () => useData<Genre>('/genres');
 const useGenres = () => useQuery({
     queryKey: ['genres'],
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,14 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-cilent";
 import ms from "ms";
+import { Platform } from "../entities/Platform";
 
 const apiCilent = new APIClient<Platform>('/platforms/list/parents');
-export interface Platform {
-    id:number;
-    name:string;
-    slug:string
-}
-
 const usePlatforms = () => useQuery({
     queryKey: ['platforms'],
     queryFn:apiCilent.getAll,
