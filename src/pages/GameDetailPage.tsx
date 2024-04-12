bc0c1a5575a61394bc9875cbd97d1549b75928e6
HectorRussia <Ponkrit.wo@hotmail.com>
2024-04-13 02:02:51 +0700

    Improving the Layout

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 import useGame from "../hooks/useGame";
-import { Heading, Spinner} from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner} from "@chakra-ui/react";
 import ExpandableText from "../component/ExpandableText";
 import GameAttribute from "../component/GameAttribute";
 import GameTrailer from "../component/GameTrailer";
@@ -12,13 +12,17 @@ const GameDetailPage = () => {
   if(isLoading) return <Spinner/>
   if(error || !game ) throw error; //game' is possibly 'undefined
   return (
-    <>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttribute game={game}></GameAttribute>
-      <GameTrailer gameId={game.id}></GameTrailer>
-      <GameScreenshots gameId={game.id}></GameScreenshots>
-    </>
+    <SimpleGrid columns={{base:1,md:2}} spacing={5}>
+      <GridItem>
+        <Heading>{game?.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttribute game={game}></GameAttribute>
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id}></GameTrailer>
+        <GameScreenshots gameId={game.id}></GameScreenshots>
+      </GridItem>
+    </SimpleGrid>
   )
 }
 
