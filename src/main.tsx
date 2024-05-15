dd3e6e92399a3461c8e4f97fee6bdfbc13bdbb3f
HectorRussia <Ponkrit.wo@hotmail.com>
2024-05-15 17:46:02 +0700

    delete tool

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import './index.css'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import theme from './theme.ts'
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes.tsx'
 
@@ -16,7 +15,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
       <QueryClientProvider client={queryClient}>
        <RouterProvider router={router}/>
-        <ReactQueryDevtools/>
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
